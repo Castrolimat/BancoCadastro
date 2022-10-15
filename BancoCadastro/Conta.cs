ab6d7d77c39207bb1059af4aac409dc675cd42d1
Thiago Lima <thiaguinho.ro@hotmail.com>
2022-10-15 15:49:13 -0300

    Add project files.

---
+++ BancoCadastro/Conta.cs
@@ -0,0 +1,39 @@
+﻿namespace BancoCadastro
+{
+    internal class ContaBancaria
+    {
+        public string TitularConta { get; set; }
+        public string NumeroConta { get; private set; }
+        public double Saldo { get; set; }
+
+        public ContaBancaria(string titularConta, string numeroConta)//construtor
+        {
+            TitularConta = titularConta;
+            NumeroConta = numeroConta;
+
+        }
+
+        public ContaBancaria(string titularConta, string numeroConta, double saldo) : this(titularConta, numeroConta)//construtor
+        {
+            Saldo = saldo;
+        }
+
+        public override string ToString()//printar usando variavel
+        {
+            return "Titular: " + TitularConta
+                + ", Número da Conta: " + NumeroConta +
+                ", Saldo (em R$): " + Saldo.ToString("F2");
+        }
+
+        public void depositarDinheiro(double valorDeposito)
+        {
+            Saldo += valorDeposito;
+        }
+        
+        public void sacarDinheiro(double ValorSaque)
+        {
+            Saldo -= ValorSaque;
+        }
+    }
+        
+}
---
+++ BancoCadastro/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace BancoCadastro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaBancaria cliente;
+            Console.Write("Nome do titular: ");
+            string nomeTitular = Console.ReadLine();
+            Console.WriteLine();
+
+            Console.Write("Número da conta: ");
+            string numeroConta = Console.ReadLine();
+            Console.WriteLine();
+
+            Console.WriteLine("Deseja fazer o depósito inicial? s/n");
+            char resp = char.Parse(Console.ReadLine());
+            if (resp == 'n')
+            {
+                cliente = new ContaBancaria(nomeTitular, numeroConta);
+            }
+            else
+            {
+                Console.Write("Valor do depósito inicial (em R$): ");
+                double saldo = double.Parse(Console.ReadLine());
+                cliente = new ContaBancaria(nomeTitular, numeroConta, saldo);
+            }
+
+            Console.WriteLine(cliente);
+
+            Console.Write("Valor do depósito: ");
+            double valorDeposito = double.Parse(Console.ReadLine());
+            cliente.depositarDinheiro(valorDeposito);
+
+            Console.WriteLine(cliente);
+            Console.WriteLine();
+
+            Console.Write("Valor do saque: ");
+            double valorSaque = double.Parse(Console.ReadLine());
+            cliente.sacarDinheiro(valorSaque);
+
+            Console.WriteLine(cliente);
+            Console.WriteLine();
+
+        }
+    }
+
+}
